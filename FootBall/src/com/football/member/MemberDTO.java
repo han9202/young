14062de3a75de548578f527d31b2031a1a4e638a
JeonHaJean <andyou920@naver.com>
2017-02-23 11:50:28 +0900

    멤버 dto

--- FootBall/src/com/football/member/MemberDTO.java
+++ FootBall/src/com/football/member/MemberDTO.java
@@ -2,4 +2,111 @@ package com.football.member;
 
 public class MemberDTO {
 
+
+private String 	seq;
+private String	id;
+private String	pw;
+private String	name;
+private String birthday;
+
+private String email;
+private String tel;
+private String	club_seq;
+private String	gender_seq;
+private String area_seq;
+
+private String userlevel;
+
+public String getSeq() {
+	return seq;
+}
+
+public void setSeq(String seq) {
+	this.seq = seq;
+}
+
+public String getId() {
+	return id;
+}
+
+public void setId(String id) {
+	this.id = id;
+}
+
+public String getPw() {
+	return pw;
+}
+
+public void setPw(String pw) {
+	this.pw = pw;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getBirthday() {
+	return birthday;
+}
+
+public void setBirthday(String birthday) {
+	this.birthday = birthday;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getTel() {
+	return tel;
+}
+
+public void setTel(String tel) {
+	this.tel = tel;
+}
+
+public String getClub_seq() {
+	return club_seq;
+}
+
+public void setClub_seq(String club_seq) {
+	this.club_seq = club_seq;
+}
+
+public String getGender_seq() {
+	return gender_seq;
+}
+
+public void setGender_seq(String gender_seq) {
+	this.gender_seq = gender_seq;
+}
+
+public String getArea_seq() {
+	return area_seq;
+}
+
+public void setArea_seq(String area_seq) {
+	this.area_seq = area_seq;
+}
+
+public String getUserlevel() {
+	return userlevel;
+}
+
+public void setUserlevel(String userlevel) {
+	this.userlevel = userlevel;
+}
+	
+
+
+
+	
 }
